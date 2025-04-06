4a50aaa45e82fcf7639dab7fccc60cf9b96ada0c
GUDDU ALI <gudduali93@yahoo.com>
2025-04-06 07:58:29 +0530

    Updated the nav bar and experience

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -15,103 +15,101 @@ const Navbar = () => {
   };
 
   return (
-    <>
-      <Box
-        display={"flex"}
-        id="nav-menu"
-        bg={useColorModeValue("gray.100", "blue.800")}
-        w={{ base: "100%", md: "100%", lg: "100%" }}
-        padding={"5px 30px"}
-        position={"fixed"}
-        top={"0"}
-        zIndex={"3"}
-        justifyContent={"space-between"}
-        alignItems={"center"}
-        boxShadow={"rgba(0, 0, 0, 0.24) 0px 3px 8px"}
-      >
-        <Box>
-          <Link to="home" smooth={true} duration={700}>
-            {" "}
-            <Image
-              cursor={"pointer"}
-              src={img}
-              w={{ base: "28%", md: "25%", lg: "35%" }}
-              borderRadius={"40px"}
-              margin={{ base: "2px", md: "1px", lg: "5px" }}
-            />
+    <Box
+      display={"flex"}
+      id="nav-menu"
+      bg={useColorModeValue("gray.100", "blue.800")}
+      w={{ base: "100%", md: "100%", lg: "100%" }}
+      padding={"5px 30px"}
+      position={"fixed"}
+      top={"0"}
+      zIndex={"3"}
+      justifyContent={"space-between"}
+      alignItems={"center"}
+      boxShadow={"rgba(0, 0, 0, 0.24) 0px 3px 8px"}
+    >
+      <Box>
+        <Link to="home" smooth={true} duration={700}>
+          {" "}
+          <Image
+            cursor={"pointer"}
+            src={img}
+            w={{ base: "28%", md: "25%", lg: "35%" }}
+            borderRadius={"40px"}
+            margin={{ base: "2px", md: "1px", lg: "5px" }}
+          />
+        </Link>
+      </Box>
+      <Box gap={"20px"} display={"flex"}>
+        <Box gap={"20px"} display={{ base: "none", md: "none", lg: "flex" }}>
+          <Link
+            to="home"
+            smooth={true}
+            duration={500}
+            className="nav-link home"
+          >
+            <Button>Home</Button>
           </Link>
-        </Box>
-        <Box gap={"20px"} display={"flex"}>
-          <Box gap={"20px"} display={{ base: "none", md: "none", lg: "flex" }}>
-            <Link
-              to="home"
-              smooth={true}
-              duration={500}
-              className="nav-link home"
-            >
-              <Button>Home</Button>
-            </Link>
-            <Link
-              to="about"
-              smooth={true}
-              duration={500}
-              className="nav-link about"
-            >
-              <Button>About</Button>
-            </Link>
-            <Link
-              to="skills"
-              smooth={true}
-              duration={500}
-              className="nav-link skills"
-            >
-              <Button>Skills</Button>
-            </Link>
-            <Link
-              to="experience"
-              smooth={true}
-              duration={500}
-              className="nav-link experiences"
-            >
-              <Button>Experiences</Button>
-            </Link>
-            <Link
-              to="project"
-              smooth={true}
-              duration={500}
-              className="nav-link projects"
-            >
-              <Button>Projects</Button>
-            </Link>
+          <Link
+            to="about"
+            smooth={true}
+            duration={500}
+            className="nav-link about"
+          >
+            <Button>About</Button>
+          </Link>
+          <Link
+            to="skills"
+            smooth={true}
+            duration={500}
+            className="nav-link skills"
+          >
+            <Button>Skills</Button>
+          </Link>
+          <Link
+            to="experience"
+            smooth={true}
+            duration={500}
+            className="nav-link experiences"
+          >
+            <Button>Experiences</Button>
+          </Link>
+          <Link
+            to="project"
+            smooth={true}
+            duration={500}
+            className="nav-link projects"
+          >
+            <Button>Projects</Button>
+          </Link>
 
-            <Link
-              to="contact"
-              smooth={true}
-              duration={500}
-              className="nav-link contact"
+          <Link
+            to="contact"
+            smooth={true}
+            duration={500}
+            className="nav-link contact"
+          >
+            <Button>Contact</Button>
+          </Link>
+          <Button id="resume-button-1" onClick={handleclick}>
+            <a
+              className="nav-link resume"
+              id="resume-link-1"
+              href={resume}
+              download="guddu-ali-resume"
             >
-              <Button>Contact</Button>
-            </Link>
-            <Button id="resume-button-1" onClick={handleclick}>
-              <a
-                className="nav-link resume"
-                id="resume-link-1"
-                href={resume}
-                download="guddu-ali-resume"
-              >
-                Resume
-              </a>
-            </Button>
-          </Box>
-          <Box>
-            <ColorModeToggle />
-          </Box>
-          <Box display={{ base: "block", md: "block", lg: "none" }}>
-            <SideDrawer />
-          </Box>
+              Resume
+            </a>
+          </Button>
+        </Box>
+        <Box>
+          <ColorModeToggle />
+        </Box>
+        <Box display={{ base: "block", md: "block", lg: "none" }}>
+          <SideDrawer />
         </Box>
       </Box>
-    </>
+    </Box>
   );
 };
 
